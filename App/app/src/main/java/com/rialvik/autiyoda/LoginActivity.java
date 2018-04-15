6334122ae018181a5a8e489b4359d078e1045b76
Logan8192 <wsses11000@gmail.com>
2018-04-14 23:26:24 -0500

    APP - SettingsActivity Actualizado
    Opciones Agregadas:
    - Modo Noche
    - Seleccionar País
    - Seleccionar Género
    - Cerrar Sesión

--- App/app/src/main/java/com/rialvik/autiyoda/LoginActivity.java
+++ App/app/src/main/java/com/rialvik/autiyoda/LoginActivity.java
@@ -100,8 +100,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      */
     private void attemptLogin() {
         if (mAuthTask != null) {
-            Intent intentMain = new Intent(LoginActivity.this,MainActivity.class);
-            startActivity(intentMain);
+
 
         }
 
@@ -144,6 +143,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
             mAuthTask.execute((Void) null);
+            Intent intentMain = new Intent(LoginActivity.this,MainActivity.class);
+            startActivity(intentMain);
         }
     }
 
